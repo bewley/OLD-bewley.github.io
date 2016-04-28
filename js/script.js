712a68133a84f42c7e4c55ae525cac629f752ce8
Will Bewley <will.bewley@gmail.com>
2016-04-27 21:10:22 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,34 @@
+//jQuery to collapse the navbar on scroll
+var header_height  = $('.navbar').height(),
+    intro_height    = $('.intro-section').height(),
+    offset_val = intro_height + header_height;
+$(window).scroll(function() {
+  var scroll_top = $(window).scrollTop();
+    if (scroll_top >= offset_val) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+            $(".navbar-fixed-top").removeClass("navbar-transparent");
+      $(".navbar-brand").addClass("nav-brand-smaller");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+      $(".navbar-fixed-top").addClass("navbar-transparent");
+      $(".navbar-brand").removeClass("nav-brand-smaller");
+    }
+});
+
+//jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// fade out arrow & intro
 $(window).scroll(function(){
-    $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250); 
+    $(".arrow").css("opacity", 1 - $(window).scrollTop() / 500);
+  $(".intro-section .col-lg-12").css("opacity", 1 - $(window).scrollTop() / 250); 
   //250 is fade pixels
   });
